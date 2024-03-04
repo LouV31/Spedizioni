57040b238b0a848b14b77a28fc667f88da879393
LouV31 <luigi.valentino99@yahoo.it>
2024-03-04 16:44:17 +0100

    Aggiungere i file di progetto.

---
+++ Spedizioni/Migrations/20240304153139_add-migration AddClienteToDatabase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Spedizioni.Migrations
+{
+    /// <inheritdoc />
+    public partial class addmigrationAddClienteToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clienti",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Identificativo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clienti", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clienti");
+        }
+    }
+}
---
+++ Spedizioni/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Spedizioni.Models
+{
+    public class Cliente
+    {
+        public enum Tipo { Privato, Azienda }
+        [Required]
+        public int Id { get; set; }
+        public string Identificativo { get; set; }
+        [Required]
+        public string Nome { get; set; }
+    }
+}
